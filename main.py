f18ccaaea67d8b48058a371163082808f855f60c
javier <javier.99jimenezvr@gmail.com>
2023-10-09 21:14:17 -0600

    [UPDATE] method PCA

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# external dependencies
+import logging
+import os
+# internal dependencies
+from utils import constants
+
+logger = logging.getLogger()
+logger.setLevel(constants.LOGGER_LEVEL)
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_categories_data():
+    response_method = {'success': False, 'message': '', 'data': {}}
+    try:
+        data = {}
+        with open('TEST_BOOL.txt', 'r') as file:
+            lines = file.readlines()
+            headers = lines[0].strip().split()
+
+            # Iterar a través de las líneas de datos
+            for line in lines[1:]:
+                # Dividir la línea en elementos
+                elements = line.strip().split()
+
+                # Obtener el nombre del archivo y la categoría
+                file_name = elements[0]
+                category = file_name.split('_')[0]
+
+                # Obtener los valores binarios como una lista
+                values = [int(value) for value in elements[1:]]
+
+                # Agregar los datos a la categoría correspondiente en el diccionario
+                if category not in data:
+                    data[category] = []
+
+                data[category].append(values)
+
+        response_method['success'] = True
+    except Exception as error:
+        logger.exception(error)
+        response_method['message'] = str(error)
+    return response_method
---
+++ utils/analisis-de-componentes-principales/PCA_CONFIG.py
@@ -0,0 +1,14 @@
+# Author: Victor Javier Rojas Jimenez
+# Date: October 2023
+
+notes = """
+                                    ### CONSIDERACIONES ###
+    El PCA seleccionará las palabras que mejor expliquen la varianza en los datos, esto quiere decir que si existen 
+    muchas palabras y documentos es posible que se necesiten más de N (limite de palabras seleccionada) 
+    componentes para explicar una cantidad significativa de varianza.
+
+"""
+
+ENCODE_FILE_INPUT = 'latin-1'
+ENCODE_FILE_OUTPUT = 'UTF-8'
+
